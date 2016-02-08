23215e5bc7c563a67de3f595094b720bf8a082d5
Ben Cull <bjcull@gmail.com>
2016-02-08 09:43:10 +0100

    Brand new project

---
+++ src/Pinch.SDK.WebSample/Controllers/PinchController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Http;
+using Microsoft.AspNet.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Pinch.SDK.WebSample.Controllers
+{
+    public class PinchController : Controller
+    {
+        public IActionResult Callback(string code)
+        {
+            var client = new HttpClient();
+
+            return View();
+        }
+    }
+}
---
+++ src/Pinch.SDK.WebSample/ViewModels/Manage/FactorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pinch.SDK.WebSample.ViewModels.Manage
+{
+    public class FactorViewModel
+    {
+        public string Purpose { get; set; }
+    }
+}
