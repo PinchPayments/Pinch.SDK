95644a34276a0b016f56e86d694d3018bdabe886
anthonyvscode <anthonyvscode@users.noreply.github.com>
2021-05-13 09:34:39 +1000

    Update EventClient.cs

--- src/Pinch.SDK/Events/EventClient.cs
+++ src/Pinch.SDK/Events/EventClient.cs
@@ -69,7 +69,7 @@ namespace Pinch.SDK.Events
         /// <param name="pageSize">Default: 50</param>
         /// <param name="startDate">Start date for date range filtering</param>
         /// <param name="endDate">End date for date range filtering</param>
-        /// <param name="eventType">If you wish to filter by a single event type, eg `payment-created`. See https://docs.getpinch.com.au/reference#list-all-events for all available event types. </param>
+        /// <param name="eventType">If you wish to filter by a single Event type, eg `payment-created`. See https://docs.getpinch.com.au/reference#list-all-events for the list of all available event types. </param>
         /// <returns></returns>
         public async Task<Paged<Event>> GetEvents(int page = 1, int pageSize = 50, DateTime? startDate = null, DateTime? endDate = null, string eventType = "")
         {
