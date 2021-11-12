fd42e6bba67545d7e7b0d2940425b6687a22b28a
Ben Cull <bjcull@gmail.com>
2021-11-12 12:35:21 +1100

    Added currency and tax rate to transfers

--- src/Pinch.SDK/Transfers/Transfer.cs
+++ src/Pinch.SDK/Transfers/Transfer.cs
@@ -9,11 +9,13 @@ namespace Pinch.SDK.Transfers
         public string Id { get; set; }
         public DateTime TransferDate { get; set; }
         public long Amount { get; set; }
+        public string Currency { get; set; }
         public string AccountName { get; set; }
         public string AccountNumber { get; set; }
         public string Bsb { get; set; }
         public string Reference { get; set; }
         public string Status { get; set; }
         public string Type { get; set; }
+        public decimal TaxRate { get; set; }
     }
 }
