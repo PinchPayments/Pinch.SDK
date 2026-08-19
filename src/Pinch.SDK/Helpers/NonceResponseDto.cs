464a017d611d7930377d20957868fd56208485ac
Hayden Richmond <hayden@getpinch.com.au>
2026-08-19 16:42:28 +1000

    response nonceresponsedto

--- src/Pinch.SDK/Helpers/NonceResponseDto.cs
+++ src/Pinch.SDK/Helpers/NonceResponseDto.cs
@@ -1,36 +1,38 @@
 ﻿using Newtonsoft.Json;
 using Pinch.SDK.Converters;
+using System;
 using System.Collections.Generic;
 
 namespace Pinch.SDK.Helpers
 {
     /// <summary>
-    /// Represents the response containing idempotency key information from the Pinch API.
+    /// Represents the response containing nonce information from the Pinch API.
     /// 
-    /// This class is used to deserialize API responses that include idempotency-related metadata,
+    /// This class is used to deserialize API responses that include nonce-related metadata,
     /// allowing clients to determine whether a request was processed as new or replayed based on
-    /// an idempotency key.
+    /// an nonce.
     /// </summary>
-    public class IdempotencyKeyResponseDto
+    [Obsolete("Use IdempotencyKeyResponseDto instead.")]
+    public class NonceResponseDto
     {
         /// <summary>
         /// Gets or sets a value indicating whether the current request is a replay of a previously
         /// processed idempotent request.
         /// 
-        /// When true, the response data is from a previous request with the same idempotency key.
+        /// When true, the response data is from a previous request with the same nonce.
         /// When false, the request was processed as a new request.
         /// </summary>
-        public bool IsIdempotencyKeyReplay { get; set; }
+        public bool IsNonceReplay { get; set; }
 
         /// <summary>
-        /// Gets or sets the idempotency key(s) returned by the server.
+        /// Gets or sets the nonce(s) returned by the server.
         /// 
-        /// The API may return either a single idempotency key as a string or multiple keys as an array.
+        /// The API may return either a single nonce as a string or multiple keys as an array.
         /// The <see cref="SingleOrArrayConverter{T}"/> automatically converts both formats into a list
         /// for consistent handling by the client.
         /// </summary>
         [JsonConverter(typeof(SingleOrArrayConverter<string>))]
-        public List<string> IdempotencyKey { get; set; }
+        public List<string> Nonce { get; set; }
     }
 
     /// <summary>
@@ -40,7 +42,8 @@ namespace Pinch.SDK.Helpers
     /// containing both idempotency metadata and the deserialized response data of type T.
     /// </summary>
     /// <typeparam name="T">The type of data contained in the response payload.</typeparam>
-    public class IdempotencyKeyResponseDto<T> : IdempotencyKeyResponseDto
+    [Obsolete("Use IdempotencyKeyResponseDto<T> instead.")]
+    public class NonceResponseDto<T> : NonceResponseDto
     {
         /// <summary>
         /// Gets or sets the typed response data returned by the API.
